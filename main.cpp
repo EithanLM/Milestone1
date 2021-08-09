a6606fe04a54295974d4bfe93413ed0a30789a43
EithanLM <eithanlmrospsha@gmail.com>
2021-08-09 20:55:25 +0300

    Fixed memory leaks

--- main.cpp
+++ main.cpp
@@ -12,6 +12,13 @@
  * @return 0 if everything worked. If something went wrong- other numbers.
  */
 int main(int argc, char** argv) {
+	std::cout << "Enter the k from the KNN." << std::endl;
+	int k;
+	std::cin >> k;
+	if (k <= 0) {
+		std::cout << "Illegal k inserted - k must be positive." << std::endl;
+		return -1;
+	}
 	string classifiedPath = "classified.csv";
 	string unclassifiedPath = "Unclassified.csv";
 	string outputPath = "output.csv";
@@ -20,13 +27,8 @@ int main(int argc, char** argv) {
 		unclassifiedPath = argv[2];
 		outputPath = argv[3];
 	}
-	std::cout << "Enter the k from the KNN." << std::endl;
-	int k;
-	std::cin >> k;
 	CSVtoData readerClassified(classifiedPath), readerUnclassified(unclassifiedPath);
 	vector<Data> classified = readerClassified.read(), unclassified = readerUnclassified.read();
-	if (k <= 0)
-		throw std::runtime_error("Illegal k inserted - k must be positive.");
 	KNNClassifier classifier(classified);
 	vector<string> categories = classifier.classifyAll(unclassified, k);
 	CSVPrinter printer(outputPath);
